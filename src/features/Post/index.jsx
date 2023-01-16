ffd7c306b349f5a7ecd4a5840b753b60ad2317cc
gnuh96 <ntuanhung.96@gmail.com>
2023-01-16 02:01:39 +0100

    pagination post list

--- src/features/Post/index.jsx
+++ src/features/Post/index.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
 import PropTypes from 'prop-types';
 import PostList from './components/PostList';
+import Pagination from '../../components/Pagination';
 
 PostFeature.propTypes = {
 
@@ -8,27 +10,51 @@ PostFeature.propTypes = {
 
 function PostFeature(props) {
     const [postList, setPostList] = useState([]);
+    const [pagination, setPagination] = useState({
+        _page: 1,
+        _limit: 10,
+        _totalRows: 1,
+    });
+
+    const [filters, setFilters] = useState({
+        _page: 1,
+        _limit: 10,
+    })
 
     useEffect(() => {
         async function fetchPostlist() {
             try {
-                const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+                const paramsString = queryString.stringify(filters);
+                const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
                 const reponse = await fetch(requestUrl);
                 const reponseJSON = await reponse.json();
                 // console.log({ reponseJSON });
 
-                const { data } = reponseJSON;
+                const { data, pagination } = reponseJSON;
                 setPostList(data);
+                setPagination(pagination);
             } catch (error) {
                 console.log('Failed to fetch post list: ', error.message);
             }
         }
         fetchPostlist();
-    }, []);
+    }, [filters]);
+
+    function handlePageChange(newPage) {
+        // console.log('New page: ', newPage);
+        setFilters({
+            ...filters,
+            _page: newPage,
+        });
+    }
     return (
         <div>
             <h1>Post List</h1>
             <PostList postList={postList} />
+            <Pagination
+                pagination={pagination}
+                onPageChange={handlePageChange}
+            />
         </div>
     );
 }

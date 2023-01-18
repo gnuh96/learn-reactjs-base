b4644f08c388ad7cbd3d870288bacbd826f87100
gnuh96 <ntuanhung.96@gmail.com>
2023-01-18 03:43:31 +0100

    install prettier

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ function App() {
   return (
     <div className="app">
       Header
-      <p><NavLink to="/todo-list" activeClassName="active-menu">Todo</NavLink></p>
-      <p><NavLink to="/album-list" activeClassName="active-menu">Album</NavLink></p>
+      <p>
+        <NavLink to="/todo-list" activeClassName="active-menu">
+          Todo
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/album-list" activeClassName="active-menu">
+          Album
+        </NavLink>
+      </p>
       <Switch>
-        <Redirect from='/home' to="/" />
+        <Redirect from="/home" to="/" />
 
         <Route path="/" component={TodoFeature} exact />
         <Route path="/todo-list" component={TodoFeature} />

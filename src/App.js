e1527760fa3500c7a4f2f75d704759fbeeda76c2
gnuh96 <ntuanhung.96@gmail.com>
2023-01-22 15:39:43 +0100

    set up redux toolkit

--- src/App.js
+++ src/App.js
@@ -5,45 +5,45 @@ import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 import NotFound from './components/NotFound';
 import productApi from './api/productApi';
+import CounterFeature from './features/Counter2';
 
 function App() {
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const params = {
-        _limit: 10,
-      };
-      const productList = await productApi.getAll(params);
-      console.log(productList);
-    };
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const params = {
+                _limit: 10,
+            };
+            const productList = await productApi.getAll(params);
+            console.log(productList);
+        };
 
-    fetchProducts();
-  }, []);
+        fetchProducts();
+    }, []);
 
-  return (
-    <div className="app">
-      Header
-      <p>
-        <NavLink to="/todo-list" activeClassName="active-menu">
-          Todo
-        </NavLink>
-      </p>
-      <p>
-        <NavLink to="/album-list" activeClassName="active-menu">
-          Album
-        </NavLink>
-      </p>
-      <Switch>
-        <Redirect from="/home" to="/" />
+    return (
+        <div className="app">
+            Header
+            <p>
+                <NavLink to="/todo-list" activeClassName="active-menu">
+                    Todo
+                </NavLink>
+            </p>
+            <p>
+                <NavLink to="/album-list" activeClassName="active-menu">
+                    Album
+                </NavLink>
+            </p>
+            <Switch>
+                <Redirect from="/home" to="/" />
 
-        <Route path="/" component={TodoFeature} exact />
-        <Route path="/todo-list" component={TodoFeature} />
-        <Route path="/album-list" component={AlbumFeature} />
+                <Route path="/" component={CounterFeature} exact />
+                <Route path="/todo-list" component={TodoFeature} />
+                <Route path="/album-list" component={AlbumFeature} />
 
-        <Route component={NotFound} />
-      </Switch>
-      Footer
-    </div>
-  );
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,14 +8,18 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 reportWebVitals();
